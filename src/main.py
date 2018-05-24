36bf419f71ef1c40060014e455c9acf6ae2fdd69
duangsuse <fedora-opensuse@outlook.com>
2018-05-24 12:56:48 +0800

    Add vcs.xml, update source

--- src/main.py
+++ src/main.py
@@ -13,10 +13,11 @@ Copyright (C) 2018, geekapk contributors.
 License: AGPL-3.0 (see LICENSE.md for details)
 """
 
+import os
 import sys
 import time
 
-from bottle import route, run, template
+from bottle import route, run, template, hook, request, response, error, get, post, abort
 from ujson import dumps, loads
 
 # require helper modules
@@ -27,9 +28,75 @@ __author__ = 'geekapk contributors'
 __version__ = '1.0-dev'
 __license__ = 'AGPL-3.0'
 
+global token_jar
+
+
+@hook('after_request')
+def enable_cross():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+
+@hook('after_request')
+def enable_json():
+    response.content_type = 'application/json; charset=utf-8'
+
+
+@error(401)
+def error401():
+    return dumps({
+        'status': 401,
+        'message': "Unauthorized",
+        'request': request
+    })
+
+
+@error(400)
+def error400():
+    return dumps({
+        'status': 400,
+        'message': "Bad request",
+        'request': request
+    })
+
+
+@error(404)
+def error404():
+    return dumps({
+        'status': 404,
+        'message': "Resources was not found",
+        'request': request
+    })
+
+
+@error(500)
+def error500():
+    return dumps({
+        'status': 500,
+        'message': "Internal error",
+        'request': request
+    })
+
+
+@get('/')
+def welcome():
+    return dumps({
+        'server': 'geekapk',
+        'version': __version__,
+        'author': __author__
+    })
+
+
+@get('/favicon.ico')
+def favicon():
+    abort(404)
+
 
 def main():
     print('Starting GeekApk Server v{} at {}'.format(__version__, time.time()))
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 8080))
+    print('Launching from {}:{}'.format(host, port))
+    run(host=host, port=port)
 
 
 if __name__ == '__main__':  # pragma: no coverage
--- src/model.py
+++ src/model.py
@@ -3,5 +3,14 @@
 # SQL model helpers for geekapk, see main.py
 # Copyright (C) 2018  geekapk contributors
 
+import os
 import records
+
+database = os.environ.get('DB') or 'postgres://geekapk:qwertyGeekApk@localhost/geekapk'
+print("Database: ", database)
+
+
+def init():
+    global db
+    db = records.Database(database)
 
